edd854572628f2905cd781070be9f5443c9267a6
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-09-04 14:26:26 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://glowing-monstera-0eb10c.netlify.app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Marta Nowak
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/martapuszkonowak/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://glowing-monstera-0eb10c.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Marta Nowak
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/martapuszkonowak/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
